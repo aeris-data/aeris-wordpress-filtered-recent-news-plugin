b4a56f8077b024dc4b976c7475ec81a64e145b69
root <root@UMSFL122.omp.obs-mip.fr>
2017-06-22 12:54:34 +0200

    add change display for side bar

--- class-wp-widget-recent-news.php
+++ class-wp-widget-recent-news.php
@@ -4,7 +4,7 @@
 
 Plugin Name: Aeris-Widget-Filtered-News-Recent
 GitHub Plugin URI:
-Version: 0.0.3
+Version: 0.0.4
 
 */
 
@@ -26,7 +26,7 @@ class NewsRecent extends WP_Widget {
 		
 		extract ( $args );
 		
-		$tutu = $instance[ 'tutu' ] ? 'true' : 'false';
+		$tutu = $instance[ 'displayMode' ] ? 'true' : 'false';
 		
 		$title = apply_filters ( 'widget_title', $instance ['title'] );
 		$nb_posts = $instance ['nb_posts'];
@@ -45,11 +45,7 @@ class NewsRecent extends WP_Widget {
 		
 		?>
 		
-		<style>
-		aside ul li::before {
-			content: none;
-		}
-		</style>
+		
 		<?php 
 		echo $before_widget;
 		if ($title)
@@ -66,40 +62,54 @@ class NewsRecent extends WP_Widget {
 		
 			
 			if( 'on' == $instance[ $category->name]) :  
-				 $CatStrQuery != "" && $CatStrQuery .= ",";
+				$CatStrQuery != "" && $CatStrQuery .= ",";
     			$CatStrQuery .= $category->name;
 				$query = new WP_Query( array( 'category_name' => $CatStrQuery,
-												'posts_per_page'=> $nb_posts
-				) );
+											  'posts_per_page'=> $nb_posts ));
+			endif;}
+							
+	  	if( 'on' == $instance[ 'displayMode']) : 
+	  		echo "<ul>";
+	  	else:
+	 		echo "<section role='listNews' class='posts'>";
+	    endif;
+	    
+		if ( $query->have_posts() ) {
 				
-				
-						endif;}
-			
-			if ( $query->have_posts() ) {
-			
 			while ( $query->have_posts() ) {
-				$query->the_post();
-				
-				$categories = get_the_terms( $post->ID, 'category');
-				
-				?>
-
-		
- 			<ul style= 'padding-left:0px;!important' >
-				<li style ="border-bottom: 1px solid #eee;">
-				<i class="fa fa-external-link-square" ></i> <a href='<?php echo  get_post_permalink($post->ID);?>'> 
-				<?php echo get_the_title($post->ID); ?></a><br>
-				<span ><?php echo 'le '. get_the_date('j F Y'); echo ' à '. get_the_time('H').' h '.get_the_time('i');?></span>
-				</li> 
- 				</ul>  
-				
-				<?php
-			}
-			
-					} else {
-			// no posts found
-			}?>
-           
+					$query->the_post();
+					$categories = get_the_terms( $post->ID, 'category');
+	 			if( 'on' == $instance[ 'displayMode']) : ?>
+	 			             
+                	<li>
+					    <a href='<?php echo  get_post_permalink($post->ID);?>'> 
+						<?php echo get_the_title($post->ID); ?></a><br>
+						<span ><?php echo 'le '. get_the_date('j F Y'); echo ' à '. get_the_time('H').' h '.get_the_time('i');?></span>
+					</li> 
+ 				
+                <?php else: ?>
+                
+              		<div class="post-container">
+                   		<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+                   	</div>
+                
+                <?php endif;
+            }
+            
+      } else {
+            // no posts found
+            }
+            
+      if( 'on' == $instance[ 'displayMode']) : 
+      
+	  	echo "</ul>";
+      
+	  else:
+	  
+	  	echo "</section>";
+	  
+	  endif;?>
+          
        
         
         
@@ -122,19 +132,16 @@ class NewsRecent extends WP_Widget {
 		// Récupération des paramètres envoyés
 		$instance ['title'] = strip_tags ( $new_instance ['title'] );
 		$instance ['nb_posts'] = $new_instance ['nb_posts'];
-		$instance[ 'tutu' ] = $new_instance[ 'tutu' ];
+		$instance[ 'displayMode' ] = $new_instance[ 'displayMode' ];
 		
 		$categories = get_categories(array(
 				'orderby' => 'name',
-				'parent'  => 0
-		) );
+				'parent'  => 0));
 		
 		foreach ( $categories as $category ) {
 			
 			$instance[$category->name] = $new_instance[ $category->name];
 		}
-		
-		
 		
 		return $instance;
 	}
@@ -147,20 +154,14 @@ class NewsRecent extends WP_Widget {
 		$nb_posts = esc_attr ( $instance ['nb_posts'] );
 		$nb_posts = isset ( $instance ['nb_posts'] ) ? absint ( $instance ['nb_posts'] ) :5;
 		
-		
-		
 		//Récupere la liste des catégorie
 		$categories = get_categories(array(
 		'orderby' => 'name',
 		'parent'  => 0
-		) );
-	
-		
-		?>
+		) );?>
 
 		<h3>catégorie</h3>
-		<?php 
-	foreach ( $categories as $category ) {?>
+		<?php foreach ( $categories as $category ) {?>
 		
 
     <input class="checkbox" type="checkbox" 
@@ -171,29 +172,41 @@ class NewsRecent extends WP_Widget {
     	  
     <label for="<?php echo $this->get_field_id( $category->name); ?>"><?php echo $category->name ?></label><br>
 
+  <?php }?>
+
+
+	<h3>Type d'affichage</h3>
+	
 		
-		<?php 	
-		}?>
+
+    <input class="checkbox" type="checkbox" 
+    <?php  checked( $instance[ 'displayMode'], 'on' ); ?> 
+    	  id="<?php echo $this->get_field_id( 'displayMode'); ?>" 
+    	  name="<?php echo $this->get_field_name( 'displayMode'); ?>" 
+    	 /> 
+    	  
+    <label for="<?php echo $this->get_field_id( 'displayMode'); ?>"><?php echo "sidebar" ?></label><br>
 
 		
-<p>
-	<label for="<?php echo $this->get_field_id('title'); ?>">
-                <?php echo 'Titre:'; ?>
-                <input class="widefat"
+	<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>">
+        	<?php echo 'Titre:'; ?>
+        <input 
+        class="widefat"
 		id="<?php echo $this->get_field_id('title'); ?>"
 		name="<?php echo $this->get_field_name('title'); ?>" type="text"
 		value="<?php echo $title; ?>" />
-	</label>
-</p>
-<p>
-<p>
-	<label for="<?php echo $this->get_field_id( 'nb_posts' ); ?>"><?php _e( 'Number of posts to show:' ); ?> 
-            	<input style="width: 20%;"
+	   </label>
+	</p>
+	
+
+	<p>
+		<label for="<?php echo $this->get_field_id( 'nb_posts' ); ?>"><?php _e( 'Number of posts to show:' ); ?> 
+        <input style="width: 20%;"
 		id="<?php echo $this->get_field_id( 'nb_posts' ); ?>"
 		name="<?php echo $this->get_field_name( 'nb_posts' ); ?>"
 		type="number" step="1" min="1" value="<?php echo $nb_posts; ?>" /> </label>
-
-</p>
+   </p>
 
 
 <?php
